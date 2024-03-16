c7428b00455be3eaf50ad5636da428fe1151da65
camara9126 <acvision01@gmail.com>
2024-03-16 16:16:17 +0000

    ajout des fichiers du dossier 'partial'

---
+++ partial/navbar.php
@@ -0,0 +1,23 @@
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+    <h3 class="navbar-brand" href="#">Fast-<span class="text-warning">Food</span></h3>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <h2 class="text-white">Welcome To The Little Taco Shop </h2>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link " href="?page=home">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=menu">Our Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=houre">Store Houres</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=contact">Contact Us</a>
+        </li>
+        
+      </ul>
+      
+    </div>
+  </nav>
---
+++ partial/pied.php
@@ -0,0 +1,13 @@
+
+    <footer">
+        <div class="col" id="copy">
+        copyright &copy; <b>The Little Taco Shop</b>
+        </div>
+    </footer>
+</div>
+
+    <script src="../assets/jquery.js"></script>
+    <script src="../assets/bootstrap.min.js"></script>
+    
+</body>
+</html>
